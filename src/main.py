f8baa2c47acdf03c57db98575afa619608197783
souz4s <psouza.1650@gmail.com>
2022-11-23 12:16:37 -0300

    feat: sending message

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
+from time import sleep
+
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 def getInput():
@@ -11,6 +17,13 @@ def browserStart(phoneNumber, message):
     driver = webdriver.Edge(executable_path=r"../../drivers/msedgedriver.exe")
     driver.get("https://web.whatsapp.com/send/?phone=" +
                phoneNumber + "&text&type=phone_number&app_absent=0")
+    sendMessage(driver, message)
+
+
+def sendMessage(driver, message):
+    WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "fd365im1"))).send_keys(message, Keys.ENTER)
+    sleep(1)
 
 
 getInput()
