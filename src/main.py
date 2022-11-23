c466cc34f1f60e217cfc1d1d0df9e84dd1c54087
souz4s <psouza.1650@gmail.com>
2022-11-23 11:03:11 -0300

    refactor: number search url change

--- src/main.py
+++ src/main.py
@@ -1,14 +1,13 @@
 from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.by import By
 
 phoneNumber = input("Enter phone number: ")
 message = input("Enter the message you want to send: ")
 
 
 def browserStart(phoneNumber, message):
-    driver = webdriver.Edge(executable_path="../../drivers/msedgedriver.exe")
-    driver.get("https://wa.me/" + phoneNumber)
+    driver = webdriver.Edge(executable_path=r"../../drivers/msedgedriver.exe")
+    driver.get("https://web.whatsapp.com/send/?phone=" +
+               phoneNumber + "&text&type=phone_number&app_absent=0")
 
 
 browserStart(phoneNumber, message)
