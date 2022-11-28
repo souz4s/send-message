20d710bedbea0582bd8f5359ed2930e767abfc86
souz4s <p.uzah@outlook.com>
2022-11-27 23:33:12 -0300

    perf: xpath file removed and added in main file

--- src/main.py
+++ src/main.py
@@ -6,8 +6,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-from xpath import xpathList
-
 
 def getInput():
     phoneNumber = input("Enter phone number: ")
@@ -26,6 +24,12 @@ def sendMessage(driver, message):
     WebDriverWait(driver, 20).until(
         EC.presence_of_element_located((By.CLASS_NAME, "fd365im1"))).send_keys(message, Keys.ENTER)
     disconnect(driver)
+
+
+def xpathList():
+    return ["//*[@id='app']/div/div/div[3]/header/div[2]/div/span/div[3]/div",
+            "//*[@id='app']/div/div/div[3]/header/div[2]/div/span/div[3]/span/div/ul/li[4]/div",
+            "//*[@id='app']/div/span[2]/div/div/div/div/div/div/div[3]/div/div[2]/div/div"]
 
 
 def disconnect(driver):
--- src/xpath.py
+++
@@ -1,4 +0,0 @@
-def xpathList():
-    return ["//*[@id='app']/div/div/div[3]/header/div[2]/div/span/div[3]/div",
-            "//*[@id='app']/div/div/div[3]/header/div[2]/div/span/div[3]/span/div/ul/li[4]/div",
-            "//*[@id='app']/div/span[2]/div/div/div/div/div/div/div[3]/div/div[2]/div/div"]
