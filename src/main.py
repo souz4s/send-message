9206dcde3cb5da51e1f2321cb15af0cac365d771
souz4s <p.uzah@outlook.com>
2022-12-21 01:12:03 -0300

    refactor: refactoring following python best practices

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from time import sleep
+import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -7,36 +7,36 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 
-def getInput():
-    phoneNumber = input("Enter phone number: ")
+def get_input():
+    phone_number = input("Enter phone number: ")
     message = input("Enter the message you want to send: ")
-    browserStart(phoneNumber, message)
+    browser_start(phone_number, message)
 
 
-def browserStart(phoneNumber, message):
+def browser_start(phone_number, message):
     driver = webdriver.Edge(executable_path=r"../../drivers/msedgedriver.exe")
     driver.get("https://web.whatsapp.com/send/?phone=" +
-               phoneNumber + "&text&type=phone_number&app_absent=0")
-    sendMessage(driver, message)
+               phone_number + "&text&type=phone_number&app_absent=0")
+    send_message(driver, message)
 
 
-def sendMessage(driver, message):
+def send_message(driver, message):
     WebDriverWait(driver, 60).until(
         EC.presence_of_element_located((By.CLASS_NAME, "fd365im1"))).send_keys(message, Keys.ENTER)
     disconnect(driver)
 
 
-def xpathList():
+def xpath_list():
     return ["//*[@id='app']/div/div/div[3]/header/div[2]/div/span/div[3]/div",
             "//*[@id='app']/div/div/div[3]/header/div[2]/div/span/div[3]/span/div/ul/li[4]/div",
             "//*[@id='app']/div/span[2]/div/div/div/div/div/div/div[3]/div/div[2]/div/div"]
 
 
 def disconnect(driver):
-    for path in xpathList():
-        sleep(0.5)
+    for path in xpath_list():
+        time.sleep(0.5)
         WebDriverWait(driver, 5).until(
             EC.presence_of_element_located((By.XPATH, path))).click()
 
 
-getInput()
+get_input()
